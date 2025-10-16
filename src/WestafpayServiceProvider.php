c4cd050b076028bbfda286b3ad86900f7ff3bb7f
Kepsondiaz <67605105+Kepsondiaz@users.noreply.github.com>
2025-10-16 23:55:35 +0000

    Fix styling

--- src/WestafpayServiceProvider.php
+++ src/WestafpayServiceProvider.php
@@ -3,9 +3,6 @@
 namespace Laravelsn\Westafpay;
 
 use Illuminate\Support\ServiceProvider;
-use Spatie\LaravelPackageTools\Package;
-use Laravelsn\Westafpay\Commands\WestafpayCommand;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class WestafpayServiceProvider extends ServiceProvider
 {
